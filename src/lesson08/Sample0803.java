aa20ca20155b04ad54d44450774396bbfe723dbc
densuke5G <danie1.apcb6160@gmail.com>
2023-11-09 14:15:04 +0900

    if nest

--- src/lesson08/Sample0803.java
+++ src/lesson08/Sample0803.java
@@ -4,21 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Sample0801 {
+public class Sample0803 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)throws IOException {
 		System.out.println("整数を入力してください");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 		String str = reader.readLine();
 		int res = Integer.parseInt(str);
 
-		if (res == 5) {
-			System.out.println("5が入力された");
-			System.out.println("式の評価値はTrue");
+		if (res > 4) {
+			if (res <= 10) {
+				System.out.println(res + "は4より大きく10以下");
+			}
 		}
 		System.out.println("終了します");
-
 	}
-
 }
